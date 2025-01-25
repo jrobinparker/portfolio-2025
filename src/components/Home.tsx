03cb08f8c6c98656dbab95c3c44d7f28ee7a5f74
Jeremy Parker <jrobinparker@gmail.com>
2025-01-24 17:58:14 -0800

    Refactor home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,35 +9,35 @@ export default function Home() {
         Jeremy Parker
       </h1>
       <div className="grid grid-cols-2 gap-10 self-center place-items-center mt-20">
-        <span
-          className="text-4xl cursor-pointer tracking-tight font-semibold hover:animate-pulse"
+        <h1
+          className="text-4xl cursor-pointer hover:animate-pulse"
           onClick={() => handleClick("about")}
         >
           about me
-        </span>
-        <span
-          className="text-4xl cursor-pointer tracking-tight font-semibold hover:animate-pulse"
+        </h1>
+        <h1
+          className="text-4xl cursor-pointer hover:animate-pulse"
           onClick={() => handleClick("exp")}
         >
           experience
-        </span>
-        <span
-          className="text-4xl cursor-pointer tracking-tight font-semibold hover:animate-pulse"
+        </h1>
+        <h1
+          className="text-4xl cursor-pointer hover:animate-pulse"
           onClick={() => handleClick("proj")}
         >
           projects
-        </span>
+        </h1>
         <a
           href="https://www.linkedin.com/in/jrobinparker"
           target="blank"
-          className="text-4xl cursor-pointer tracking-tight font-semibold hover:animate-pulse"
+          className="text-4xl tracking-tight font-semibold hover:animate-pulse"
         >
           linkedin
         </a>
         <a
           href="https://www.github.com/jrobinparker"
           target="blank"
-          className="text-4xl cursor-pointer tracking-tight font-semibold col-span-2 hover:animate-pulse"
+          className="text-4xl tracking-tight font-semibold col-span-2 hover:animate-pulse"
         >
           github
         </a>
