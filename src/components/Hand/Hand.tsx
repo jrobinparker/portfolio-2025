5f0c547ba0a5337f873ed54d794ea19835777f98
Jeremy Parker <jrobinparker@gmail.com>
2025-01-29 14:35:37 -0800

    Fix placement of Hand component

--- src/components/Hand/Hand.tsx
+++ src/components/Hand/Hand.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 
 export default function Hand() {
   return (
-    <div className="h-screen text-center">
+    <div className="h-screen text-center inline-block w-full place-content-center">
       <div className="text-7xl mb-10 wave">👋</div>
       <h1 className="text-4xl">thanks for stopping by!</h1>
     </div>
